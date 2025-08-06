441ef0061c69ed099a5622032e1a217f3a1848e9
SANTHOSH R <ravisanthosh0662@gmail.com>
2025-08-06 20:25:07 +0530

    Create output log for full-adder.sv

---
+++ FULL-ADDER/output log for full-adder.sv
@@ -0,0 +1,25 @@
+Contains Synopsys proprietary information.
+Compiler version U-2023.03-SP2_Full64; Runtime version U-2023.03-SP2_Full64;  Aug  6 10:45 2025
+fr_gen
+                   0a=0,b=1,cin=1,sum=0,carry=0
+fr_driv
+                   1a=0,b=1,cin=1,sum=0,carry=0
+fr_mon
+                   2a=0,b=1,cin=1,sum=0,carry=1
+fr_scr_b
+                   2a=0,b=1,cin=1,sum=0,carry=1
+Result:pass
+fr_gen
+                   3a=1,b=0,cin=0,sum=0,carry=0
+fr_driv
+                   4a=1,b=0,cin=0,sum=0,carry=0
+fr_mon
+                   5a=1,b=0,cin=0,sum=1,carry=0
+fr_scr_b
+                   5a=1,b=0,cin=0,sum=1,carry=0
+Result:pass
+$finish at simulation time                    6
+           V C S   S i m u l a t i o n   R e p o r t 
+Time: 6 ns
+CPU Time:      0.520 seconds;       Data structure size:   0.0Mb
+Wed Aug  6 10:45:43 2025
